ed1be9e495e32c3f21e343ef4ac9d7fc1e872768
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2019-11-29 22:22:21 +0100

    Initial commit

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using Entities;
+using Entities.Helpers;
+using Entities.Models;
+using System;
+using System.Linq;
+
+namespace Repository
+{
+	public class AccountRepository : RepositoryBase<Account>, IAccountRepository
+	{
+		public AccountRepository(RepositoryContext repositoryContext)
+			: base(repositoryContext)
+		{
+
+		}
+
+		public PagedList<Account> GetAccountsByOwner(Guid ownerId, AccountParameters parameters)
+		{
+			var accounts = FindByCondition(a => a.OwnerId.Equals(ownerId));
+
+			return PagedList<Account>.ToPagedList(accounts,
+				parameters.PageNumber,
+				parameters.PageSize);
+		}
+
+		public Account GetAccountByOwner(Guid ownerId, Guid id)
+		{
+			return FindByCondition(a => a.OwnerId.Equals(ownerId) && a.Id.Equals(id)).SingleOrDefault();
+		}
+	}
+}
