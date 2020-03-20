5623161ae24722e2c3d5b60a1dd90790577b92fe
CodeMazeBlog <contact@code-maze.com>
2020-03-20 13:03:10 +0100

    ToPagedList fix.

--- Entities/Helpers/PagedList.cs
+++ Entities/Helpers/PagedList.cs
@@ -24,7 +24,7 @@ namespace Entities.Helpers
 			AddRange(items);
 		}
 
-		public static PagedList<T> ToPagedList(IEnumerable<T> source, int pageNumber, int pageSize)
+		public static PagedList<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
 		{
 			var count = source.Count();
 			var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
